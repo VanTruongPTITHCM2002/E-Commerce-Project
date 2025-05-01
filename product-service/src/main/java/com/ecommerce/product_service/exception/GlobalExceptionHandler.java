00433b3d022ba61d7ad2d41794a544d4f79a39be
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-05-01 15:47:47 +0700

    feat(product): add exception methodagrumentnotvalid order to catch not valid product

--- product-service/src/main/java/com/ecommerce/product_service/exception/GlobalExceptionHandler.java
+++ product-service/src/main/java/com/ecommerce/product_service/exception/GlobalExceptionHandler.java
@@ -3,9 +3,12 @@ package com.ecommerce.product_service.exception;
 import com.ecommerce.product_service.dto.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +20,18 @@ public class GlobalExceptionHandler {
                         .status(HttpStatus.NOT_FOUND.value())
                         .message("Not found method you access")
                         .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<String>> handleMethodArgumentNotValidException (MethodArgumentNotValidException methodArgumentNotValidException) {
+        String error = Objects.requireNonNull(methodArgumentNotValidException.getFieldError()).getDefaultMessage();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(
+                        ApiResponse.<String>builder()
+                                .status(HttpStatus.BAD_REQUEST.value())
+                                .message(error)
+                                .build()
+                );
     }
 }
