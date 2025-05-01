65c4a743b2e1e7b6cec58ae89e74e36147a38d70
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-05-01 15:46:59 +0700

    feat(product): add validation product request

--- product-service/src/main/java/com/ecommerce/product_service/dto/request/ProductRequest.java
+++ product-service/src/main/java/com/ecommerce/product_service/dto/request/ProductRequest.java
@@ -1,6 +1,9 @@
 package com.ecommerce.product_service.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -11,7 +14,11 @@ import lombok.experimental.FieldDefaults;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductRequest {
+    @Size(min = 5, message = "Product name must be at least 5 characters")
+    @NotEmpty(message = "Please don't empty product name")
     String name;
+    @Min(value = 0, message = "Price must be equal greater than 0")
     int price;
+    @Min(value = 0, message = "Quantity must be equal greater than 0")
     int quantity;
 }
