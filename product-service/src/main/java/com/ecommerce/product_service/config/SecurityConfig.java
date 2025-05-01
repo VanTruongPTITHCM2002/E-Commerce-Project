45b52f1bd8d42a2ad1226e636fd93e9ce6b35b1c
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-05-01 16:51:23 +0700

    feat(product): add security add check token, add param application.properties equal .env

--- product-service/src/main/java/com/ecommerce/product_service/config/SecurityConfig.java
+++ product-service/src/main/java/com/ecommerce/product_service/config/SecurityConfig.java
@@ -1,4 +1,53 @@
 package com.ecommerce.product_service.config;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Value("${jwt.secret-key}")
+    private String SECRET_KEY;
+
+    private final Environment env;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> {
+                            request.requestMatchers(HttpMethod.POST,env.getProperty("API_URL_PRODUCT")).authenticated()
+                                    .requestMatchers(HttpMethod.PUT,env.getProperty("API_URL_PRODUCT_PARAM")).authenticated()
+                                    .requestMatchers(HttpMethod.DELETE,env.getProperty("API_URL_PRODUCT_PARAM")).authenticated()
+                                    .anyRequest().permitAll();
+                }) .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer
+                        .decoder(jwtDecoder())))
+                .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(SECRET_KEY.getBytes(),"HS512");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
 }
