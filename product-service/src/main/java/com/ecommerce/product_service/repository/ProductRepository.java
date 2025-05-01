92cfa542f33ce9e1a93219fd674e36d2f7964945
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-05-01 15:58:46 +0700

    feat(product): add @NonNull method findAll

--- product-service/src/main/java/com/ecommerce/product_service/repository/ProductRepository.java
+++ product-service/src/main/java/com/ecommerce/product_service/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.ecommerce.product_service.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -11,7 +12,9 @@ import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     @Override
-    Page<Product> findAll(Pageable pageable);
+    @NonNull
+    Page<Product> findAll(@NonNull Pageable pageable);
+
     Optional<Product> findByName(String name);
     boolean existsByName(String name);
 }
