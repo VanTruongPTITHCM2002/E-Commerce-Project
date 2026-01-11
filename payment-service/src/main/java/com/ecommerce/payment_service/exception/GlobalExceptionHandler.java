b9f33d2b90e502cca82a5b9c2dcb0a68dc941389
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-11 12:06:35 +0700

    feat(payment-service): add-exception

--- payment-service/src/main/java/com/ecommerce/payment_service/exception/GlobalExceptionHandler.java
+++ payment-service/src/main/java/com/ecommerce/payment_service/exception/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.ecommerce.payment_service.exception;
 
+import com.ecommerce.payment_service.dto.reponse.ApiResponse;
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FeignException.Unauthorized.class)
+    public ResponseEntity<ApiResponse<String>> handleFeignExceptionUnauthorized(FeignException.Unauthorized unauthorized){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ApiResponse.<String>builder()
+                        .status(HttpStatus.UNAUTHORIZED.value())
+                        .message("You must login")
+                        .build());
+    }
 }
