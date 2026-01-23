b9a67319cdba23fdfc1355c64f3107a4abcaca3e
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-23 14:04:44 +0700

    feat(order-service): remove-find-all-in-order-repository

--- order-service/src/main/java/com/ecommerce/order_service/repository/OrderRepository.java
+++ order-service/src/main/java/com/ecommerce/order_service/repository/OrderRepository.java
@@ -1,19 +1,13 @@
 package com.ecommerce.order_service.repository;
 
 import com.ecommerce.order_service.entity.Order;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-    @Override
-    @NonNull
-    Page<Order> findAll(@NonNull Pageable pageable);
-
+public interface OrderRepository extends JpaRepository<Order,Integer>, JpaSpecificationExecutor<Order> {
     List<Order> findByUserId (String userId);
 }
