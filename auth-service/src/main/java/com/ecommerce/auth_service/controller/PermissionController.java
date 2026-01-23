bdc9104cdfb807a6ed996d6f3794d9afbfb7383d
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-23 14:07:52 +0700

    feat(auth-service): add-enum-message-in-permission-controller

--- auth-service/src/main/java/com/ecommerce/auth_service/controller/PermissionController.java
+++ auth-service/src/main/java/com/ecommerce/auth_service/controller/PermissionController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.auth_service.controller;
 
+import com.ecommerce.auth_service.common.ResponseMessageSuccess;
 import com.ecommerce.auth_service.constants.RoleConstants;
 import com.ecommerce.auth_service.dto.request.PermissionRequest;
 import com.ecommerce.auth_service.dto.response.ApiResponse;
@@ -30,34 +31,34 @@ public class PermissionController {
     @PreAuthorize(RoleConstants.ADMIN)
     public ResponseEntity<ApiResponse<List<PermissionResponse>>> getPermissions(){
         List<PermissionResponse> responses = this.iPermissionService.getPermissions();
-        return ResponseUtils.ok("Get permission successfully", responses);
+        return ResponseUtils.ok(ResponseMessageSuccess.PERMISSIONS_LISTED.getMessage(), responses);
     }
 
     @GetMapping("/{permissionId}")
     @PreAuthorize(RoleConstants.ADMIN)
     public ResponseEntity<ApiResponse<PermissionResponse>> getPermissionById (@PathVariable int permissionId){
         PermissionResponse permissionResponse = this.iPermissionService.getPermissionById(permissionId);
-        return ResponseUtils.ok("Get permission successfully", permissionResponse);
+        return ResponseUtils.ok(ResponseMessageSuccess.PERMISSION_FOUND.getMessage(), permissionResponse);
     }
 
     @PostMapping
     @PreAuthorize(RoleConstants.ADMIN)
     public  ResponseEntity<ApiResponse<PermissionResponse>> addPPermission (@RequestBody PermissionRequest permissionRequest){
         PermissionResponse response = this.iPermissionService.addPermission(permissionRequest);
-        return ResponseUtils.create("Created permission successfully", response);
+        return ResponseUtils.create(ResponseMessageSuccess.PERMISSION_CREATED.getMessage(), response);
     }
 
     @PutMapping("/{permissionId}")
     @PreAuthorize(RoleConstants.ADMIN)
     public ResponseEntity<ApiResponse<PermissionResponse>> updatePermission (@PathVariable int permissionId ,@RequestBody PermissionRequest permissionRequest){
         PermissionResponse response = this.iPermissionService.updatePermission(permissionId, permissionRequest);
-        return ResponseUtils.ok("Update permission successfully", response);
+        return ResponseUtils.ok(ResponseMessageSuccess.PERMISSION_UPDATED.getMessage(), response);
     }
 
     @DeleteMapping("/{permissionId}")
     @PreAuthorize(RoleConstants.ADMIN)
     public ResponseEntity<ApiResponse<Boolean>> deletePermission (@PathVariable int permissionId){
         boolean isDeleted = this.iPermissionService.deletePermission(permissionId);
-        return ResponseUtils.ok("Delete permission " + (isDeleted ? "successfully" : "failed"), isDeleted);
+        return ResponseUtils.ok(ResponseMessageSuccess.PERMISSION_DELETED.getMessage(), isDeleted);
     }
 }
