1d78eeca8c851ab54ef1b3eac120761a9ecdd9a7
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-23 13:58:46 +0700

    feat(order-service): add-authenticate-interceptor-in-cart-client

--- order-service/src/main/java/com/ecommerce/order_service/event/CartServiceClient.java
+++ order-service/src/main/java/com/ecommerce/order_service/event/CartServiceClient.java
@@ -1,11 +1,12 @@
 package com.ecommerce.order_service.event;
 
+import com.ecommerce.order_service.config.AuthenticationRequestInterceptor;
 import com.ecommerce.order_service.dto.response.ApiResponse;
 import com.ecommerce.order_service.dto.response.CartResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "cart-service", url = "http://localhost:8084/api/v1/")
+@FeignClient(name = "cart-service", url = "http://localhost:8084/api/v1/", configuration = AuthenticationRequestInterceptor.class)
 public interface CartServiceClient {
     @GetMapping("/{userId}/cart")
     ApiResponse<CartResponse> getCartByUser (@PathVariable String userId);
