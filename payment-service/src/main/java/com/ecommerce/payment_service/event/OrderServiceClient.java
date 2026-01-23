c7d231f40428e60dcbd22f7d42768bcefe9251eb
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-23 14:05:53 +0700

    feat(payment-service): add-authenticate-interceptor-in-order-client

--- payment-service/src/main/java/com/ecommerce/payment_service/event/OrderServiceClient.java
+++ payment-service/src/main/java/com/ecommerce/payment_service/event/OrderServiceClient.java
@@ -1,6 +1,7 @@
 package com.ecommerce.payment_service.event;
 
 import com.ecommerce.order_service.Enum.OrderStatus;
+import com.ecommerce.order_service.config.AuthenticationRequestInterceptor;
 import com.ecommerce.payment_service.dto.reponse.ApiResponse;
 import com.ecommerce.payment_service.dto.reponse.OrderResponse;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name ="order-service" ,url = "http://localhost:8082/api/v1")
+@FeignClient(name ="order-service" ,url = "http://localhost:8082/api/v1", configuration = AuthenticationRequestInterceptor.class)
 public interface OrderServiceClient {
     @GetMapping("/orders/{orderId}")
     ApiResponse<OrderResponse> getOrderByOrderId(@PathVariable int orderId);
 
     @PutMapping("/orders/{orderId}/status")
-    public ResponseEntity<ApiResponse<String>> updateOrderStatus (@PathVariable int orderId
+    ResponseEntity<ApiResponse<String>> updateOrderStatus (@PathVariable int orderId
             , @RequestParam String orderStatus);
 }
