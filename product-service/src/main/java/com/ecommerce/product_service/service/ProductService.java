b02f3738dd4020c5b1133367e2f50fe878e259b0
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-23 13:56:37 +0700

    chore(product-service): add-function-get-products-paginate-in-interface

--- product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
+++ product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
@@ -2,11 +2,14 @@ package com.ecommerce.product_service.service;
 
 
 import com.ecommerce.product_service.dto.request.ProductRequest;
+import com.ecommerce.product_service.dto.response.PageResponse;
 import com.ecommerce.product_service.dto.response.ProductAdminResponse;
 import com.ecommerce.product_service.dto.response.ProductResponse;
 import com.ecommerce.product_service.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,6 +17,7 @@ import java.time.LocalDateTime;
 
 public interface ProductService {
     Page<ProductAdminResponse> getProducts(Pageable pageable, BigInteger minPrice, BigInteger maxPrice, String keyWord, Boolean isDeleted, LocalDate startDate, LocalDate endDate);
+    PageResponse<ProductAdminResponse> getProducts (Specification<Product> specification, Pageable pageable, String filter);
     Page<ProductResponse> getPublishProduct (Pageable pageable, BigInteger minPrice, BigInteger maxPrice, String keyword);
     ProductAdminResponse insertProduct(ProductRequest productDto);
     ProductAdminResponse getProductById(String productId);
