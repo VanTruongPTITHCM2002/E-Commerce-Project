a35ebd0e565024ce9e496a933764e32d2f411fe3
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-23 14:14:11 +0700

    feat(auth-service): remove-find-all-in-user-repository

--- auth-service/src/main/java/com/ecommerce/auth_service/repository/UserRepository.java
+++ auth-service/src/main/java/com/ecommerce/auth_service/repository/UserRepository.java
@@ -1,20 +1,15 @@
 package com.ecommerce.auth_service.repository;
 
 import com.ecommerce.auth_service.entity.User;
-import lombok.NonNull;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User,String> {
-    @Override
-    @NonNull
-    Page<User> findAll(@NonNull Pageable pageable);
-
+@Repository
+public interface UserRepository extends JpaRepository<User,String>, JpaSpecificationExecutor<User> {
     Optional<User> findByUsername(@Param("username") String username);
     boolean existsByUsername (String username);
     boolean existsByEmail (String email);
