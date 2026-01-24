49fec7b7eb0b65c3f061783e4d13417400eecbf9
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-24 13:26:36 +0700

    feat(product-service): update-function-get-products-admin-in-interface

--- product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
+++ product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
@@ -11,12 +11,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.math.BigInteger;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 
 public interface ProductService {
-    Page<ProductAdminResponse> getProducts(Pageable pageable, BigInteger minPrice, BigInteger maxPrice, String keyWord, Boolean isDeleted, LocalDate startDate, LocalDate endDate);
+    PageResponse<ProductAdminResponse> getAdminProducts(Specification<Product> specification, Pageable pageable, String filter);
     PageResponse<ProductAdminResponse> getProducts (Specification<Product> specification, Pageable pageable, String filter);
     Page<ProductResponse> getPublishProduct (Pageable pageable, BigInteger minPrice, BigInteger maxPrice, String keyword);
     ProductAdminResponse insertProduct(ProductRequest productDto);
