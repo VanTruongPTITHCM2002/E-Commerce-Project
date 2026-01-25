d6c233b8dd6aac4b2ed92f1d4c0b08d95ff77746
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-25 18:38:12 +0700

    feat(product-service): add-function-exists-in-brand-repository

--- product-service/src/main/java/com/ecommerce/product_service/repository/BrandRepository.java
+++ product-service/src/main/java/com/ecommerce/product_service/repository/BrandRepository.java
@@ -3,8 +3,12 @@ package com.ecommerce.product_service.repository;
 import com.ecommerce.product_service.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface BrandRepository extends JpaRepository<Brand, UUID>, JpaSpecificationExecutor<Brand> {
+    boolean  existsByName (String name);
+    boolean existsBySlug (String slug);
 }
