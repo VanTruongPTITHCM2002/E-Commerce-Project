e41585f169c4590f53c8c886abe7ea5aea75cc15
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-25 16:07:39 +0700

    feat(product-service): add-mapping-and-update-in-brand-mapper

--- product-service/src/main/java/com/ecommerce/product_service/mapper/BrandMapper.java
+++ product-service/src/main/java/com/ecommerce/product_service/mapper/BrandMapper.java
@@ -1,14 +1,20 @@
 package com.ecommerce.product_service.mapper;
 
 import com.ecommerce.product_service.dto.request.BrandRequest;
+import com.ecommerce.product_service.dto.request.BrandUpdateRequest;
 import com.ecommerce.product_service.dto.response.BrandResponse;
 import com.ecommerce.product_service.entity.Brand;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface BrandMapper {
 
     Brand toEntity (BrandRequest brandRequest);
 
+    @Mapping(source = "code", target = "code")
     BrandResponse toResponse (Brand brand);
+
+    Brand toUpdate (@MappingTarget Brand brand, BrandUpdateRequest brandUpdateRequest);
 }
