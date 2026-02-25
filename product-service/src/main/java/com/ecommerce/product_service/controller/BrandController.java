0dafa83f9e3b6d2b2e157ade07ff79a4405a1e2f
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-25 16:06:38 +0700

    feat(product-service): add-brand-controller-infinity-and-slugs-and-code

--- product-service/src/main/java/com/ecommerce/product_service/controller/BrandController.java
+++ product-service/src/main/java/com/ecommerce/product_service/controller/BrandController.java
@@ -1,10 +1,12 @@
 package com.ecommerce.product_service.controller;
 
 import com.ecommerce.product_service.dto.request.BrandRequest;
+import com.ecommerce.product_service.dto.request.BrandUpdateRequest;
 import com.ecommerce.product_service.dto.response.ApiResponse;
 import com.ecommerce.product_service.dto.response.BrandResponse;
 import com.ecommerce.product_service.dto.response.PageResponse;
 import com.ecommerce.product_service.entity.Brand;
+import com.ecommerce.product_service.enums.MessageSuccess;
 import com.ecommerce.product_service.service.BrandService;
 import com.ecommerce.product_service.utils.ResponseUtils;
 import com.turkraft.springfilter.boot.Filter;
@@ -17,8 +19,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/brands")
@@ -29,30 +36,56 @@ public class BrandController {
     BrandService brandService;
 
     @GetMapping
+//    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<ApiResponse<PageResponse<BrandResponse>>> getBrandsPaginate (
             @Filter Specification<Brand> specification,
-            @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable,
+            @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             @RequestParam(value = "filter", defaultValue = "null") String filter
-            ) {
+    ) {
             PageResponse<BrandResponse> responses = this.brandService.getBrands(specification, pageable, filter);
-            return ResponseUtils.ok("Get brands paginate successfully",responses);
+            return ResponseUtils.ok(MessageSuccess.BRAND_LIST_RETRIEVED.getMessage(), responses);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<BrandResponse>> getBrandById (@PathVariable("id") String id) {
         BrandResponse response = this.brandService.getBrandById(id);
-        return ResponseUtils.ok("Get brand with id: " + id + " successfully", response);
+        return ResponseUtils.ok(MessageSuccess.BRAND_RETRIEVED_SUCCESSFULLY.getMessage(), response);
+    }
+
+    @GetMapping("/select/infinity")
+    public ResponseEntity<ApiResponse<List<BrandResponse>>> getBrandSelectInfinity() {
+        List<BrandResponse> responses = this.brandService.selectBrandInfinity();
+        return ResponseUtils.ok(MessageSuccess.BRAND_SELECT_INFINITY_SUCCESSFULLY.getMessage(), responses);
+    }
+
+    @GetMapping("/slugs")
+    public ResponseEntity<ApiResponse<Map<UUID, String>>> getSlugs (@RequestParam("name") String slug) {
+        Map<UUID, String> uuidStringMap = this.brandService.getSlugs(slug);
+        return ResponseUtils.ok(MessageSuccess.SLUG_LIST_RETRIEVED_SUCCESSFULLY.getMessage(), uuidStringMap);
+    }
+
+    @GetMapping("/code")
+    public ResponseEntity<ApiResponse<BrandResponse>> getBrandByCode (@RequestParam("value") String code) {
+        BrandResponse response = this.brandService.getByCodes(code);
+        return ResponseUtils.ok(MessageSuccess.BRAND_RETRIEVED_BY_CODE_SUCCESSFULLY.getMessage(), response);
     }
 
     @PostMapping
     public ResponseEntity<ApiResponse<BrandResponse>> createBrand (@RequestBody @Valid BrandRequest brandRequest) {
         BrandResponse brandResponse = this.brandService.createBrand(brandRequest);
-        return ResponseUtils.ok("Create brand successfully", brandResponse);
+        return ResponseUtils.ok(MessageSuccess.BRAND_CREATED_SUCCESSFULLY.getMessage(), brandResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<BrandResponse>> updateBrand (@PathVariable("id") String id,
+                                                                   @RequestBody @Valid BrandUpdateRequest brandUpdateRequest) {
+        BrandResponse brandResponse = this.brandService.updateBrand(id, brandUpdateRequest);
+        return  ResponseUtils.ok(MessageSuccess.BRAND_UPDATED_SUCCESSFULLY.getMessage(), brandResponse);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse<Void>> deleteBrand (@PathVariable("id") String id) {
         this.brandService.deleteBrand(id);
-        return ResponseUtils.ok("Delete brand successfully", null);
+        return ResponseUtils.ok(MessageSuccess.BRAND_DELETED_SUCCESSFULLY.getMessage(), null);
     }
 }
