19f68156d22ad01df18c2b8176d0408ea94afe9b
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-03 10:08:17 +0700

    feat(product-service): update-enum-message-success

--- product-service/src/main/java/com/ecommerce/product_service/enums/MessageSuccess.java
+++ product-service/src/main/java/com/ecommerce/product_service/enums/MessageSuccess.java
@@ -10,6 +10,14 @@ public enum MessageSuccess {
     PRODUCT_UPDATED_SUCCESSFULLY("Product updated successfully."),
     PRODUCT_DELETED_SUCCESSFULLY("Product deleted successfully."),
     PRODUCT_RETRIEVED_SUCCESSFULLY("Product retrieved successfully."),
-    PRODUCT_LIST_RETRIEVED("Product list retrieved successfully.");
+    PRODUCT_LIST_RETRIEVED("Product list retrieved successfully."),
+    BRAND_LIST_RETRIEVED("Brand list retrieved successfully"),
+    BRAND_CREATED_SUCCESSFULLY("Brand created successfully"),
+    BRAND_UPDATED_SUCCESSFULLY("Brand updated successfully"),
+    BRAND_DELETED_SUCCESSFULLY("Brand deleted successfully"),
+    BRAND_RETRIEVED_SUCCESSFULLY("Brand retrieved successfully"),
+    BRAND_SELECT_INFINITY_SUCCESSFULLY("Brand selected infinity successfully"),
+    SLUG_LIST_RETRIEVED_SUCCESSFULLY("Slug list retrieved successfully"),
+    BRAND_RETRIEVED_BY_CODE_SUCCESSFULLY("Brand retrieved by code successfully");
     private final String message;
 }
