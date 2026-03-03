83f238f1d8e15462e1db06c947b4b96a0df94e65
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-03 10:05:00 +0700

    feat(product-service): add-function-in-category-service

--- product-service/src/main/java/com/ecommerce/product_service/service/CategoryService.java
+++ product-service/src/main/java/com/ecommerce/product_service/service/CategoryService.java
@@ -1,4 +1,23 @@
 package com.ecommerce.product_service.service;
 
+import com.ecommerce.product_service.dto.request.CategoryRequest;
+import com.ecommerce.product_service.dto.request.CategoryUpdateRequest;
+import com.ecommerce.product_service.dto.response.CategoryDetailResponse;
+import com.ecommerce.product_service.dto.response.CategoryResponse;
+import com.ecommerce.product_service.dto.response.CategoryTreeResponse;
+import com.ecommerce.product_service.dto.response.PageResponse;
+import com.ecommerce.product_service.entity.Category;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
 public interface CategoryService {
+    CategoryResponse createCategory (CategoryRequest categoryRequest);
+    CategoryDetailResponse getCategoryById (String id);
+    PageResponse<CategoryResponse> getCategories(Specification<Category> specification, Pageable pageable, String filter);
+    CategoryResponse updateCategory (String id, CategoryUpdateRequest categoryUpdateRequest);
+    CategoryTreeResponse viewTreeCategory (String id);
+    List<CategoryTreeResponse> viewTreeCategories ();
+    void deleteCategory (String id);
 }
