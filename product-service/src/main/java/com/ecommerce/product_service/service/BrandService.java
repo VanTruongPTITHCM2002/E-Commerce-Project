ee43057f26d339073bf05fcc472e806ae00c876f
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-03 10:00:59 +0700

    feat(product-service): add-function-in-brand-service

--- product-service/src/main/java/com/ecommerce/product_service/service/BrandService.java
+++ product-service/src/main/java/com/ecommerce/product_service/service/BrandService.java
@@ -1,6 +1,7 @@
 package com.ecommerce.product_service.service;
 
 import com.ecommerce.product_service.dto.request.BrandRequest;
+import com.ecommerce.product_service.dto.request.BrandUpdateRequest;
 import com.ecommerce.product_service.dto.response.BrandResponse;
 import com.ecommerce.product_service.dto.response.PageResponse;
 import com.ecommerce.product_service.entity.Brand;
@@ -8,12 +9,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public interface BrandService {
     BrandResponse createBrand (BrandRequest brandRequest);
     PageResponse<BrandResponse> getBrands (Specification<Brand> specification,
                                            Pageable pageable, String filter);
     BrandResponse getBrandById (String id);
+    Map<UUID, String> getSlugs (String slug);
+    BrandResponse getByCodes (String code);
+    BrandResponse updateBrand (String id, BrandUpdateRequest brandUpdateRequest);
     void deleteBrand (String id);
     List<BrandResponse> selectBrandInfinity ();
 }
