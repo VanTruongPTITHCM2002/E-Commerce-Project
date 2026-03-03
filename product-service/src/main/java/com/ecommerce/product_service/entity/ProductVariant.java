e1eddd70147d886f19e2d97832d466248edeaee4
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-03 11:35:34 +0700

    feat(product-service): update-entity-product-variant

--- product-service/src/main/java/com/ecommerce/product_service/entity/ProductVariant.java
+++ product-service/src/main/java/com/ecommerce/product_service/entity/ProductVariant.java
@@ -1,10 +1,14 @@
 package com.ecommerce.product_service.entity;
 
+import io.hypersistence.utils.hibernate.type.json.JsonBinaryType;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -21,7 +25,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ProductVariant {
+public class ProductVariant extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     UUID id;
@@ -30,19 +34,29 @@ public class ProductVariant {
     @JoinColumn(name = "product_id", nullable = false)
     Product product;
 
+    @Column(unique = true)
     String sku;
 
-    @Column(name = "variant_name")
+    @Column(name = "variant_name", unique = true)
     String variantName;
 
-    @Column(precision = 15, scale = 2)
+    @Column(name = "price",nullable = false, precision = 15, scale = 2)
     BigDecimal price;
 
-    Integer stockQuantity;
+    @Column(name = "original_price", precision = 15, scale = 2)
+    BigDecimal originalPrice;
+
+    @Column(name = "cost_price", precision = 15, scale = 2)
+    BigDecimal costPrice;
+
+    @Column(name = "quantity", nullable = false, precision = 15, scale = 2)
+    BigDecimal stockQuantity;
 
     @ElementCollection
     List<String> images = new ArrayList<>();
 
+    @Type(JsonBinaryType.class)
+    @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb")
     String attributes;
 }
