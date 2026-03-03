b5fd554b4c7ee72d59748335879971a337b52e90
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-03 10:06:32 +0700

    feat(product-service): add-dto-category-update-request

---
+++ product-service/src/main/java/com/ecommerce/product_service/dto/request/CategoryUpdateRequest.java
@@ -0,0 +1,35 @@
+package com.ecommerce.product_service.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CategoryUpdateRequest {
+    @NotEmpty(message = "Category name must not be empty")
+    @NotNull(message = "Category name must not be null")
+    @NotBlank(message = "Category name must not be blank")
+    @Size(min = 4, max = 100, message = "Category name must be at least 4 characters")
+    String name;
+    @NotEmpty(message = "Slug must not be empty")
+    @NotNull(message = "Slug must not be null")
+    @NotBlank(message = "Slug must not be blank")
+    @Size(min = 4, max = 100, message = "Slug must be at least 4 characters")
+    String slug;
+    @Size(max = 1000, message = "Description is too long")
+    String description;
+    @Size(max = 1000, message = "Icon Url is too long")
+    String iconUrl;
+    String parentId;
+}
