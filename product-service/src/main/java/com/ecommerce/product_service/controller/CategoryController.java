9c855d803008a11f3bd16d70917ef290afb65a13
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-03 10:02:46 +0700

    feat(product-service): add-get-update-create-delete-in-category-controller

---
+++ product-service/src/main/java/com/ecommerce/product_service/controller/CategoryController.java
@@ -0,0 +1,79 @@
+package com.ecommerce.product_service.controller;
+
+import com.ecommerce.product_service.dto.request.CategoryRequest;
+import com.ecommerce.product_service.dto.request.CategoryUpdateRequest;
+import com.ecommerce.product_service.dto.response.*;
+import com.ecommerce.product_service.entity.Category;
+import com.ecommerce.product_service.service.CategoryService;
+import com.ecommerce.product_service.utils.ResponseUtils;
+import com.turkraft.springfilter.boot.Filter;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CategoryController {
+    CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<CategoryResponse>> createCategory (@RequestBody CategoryRequest categoryRequest) {
+        CategoryResponse response = this.categoryService.createCategory(categoryRequest);
+        return ResponseUtils.create("Create category successfully", response);
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<PageResponse<CategoryResponse>>> getCategoriesPaginate (
+            @Filter Specification<Category> specification,
+            @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            @RequestParam(value = "filter", required = false) String filter
+            ) {
+        PageResponse<CategoryResponse> responses = this.categoryService.getCategories(specification, pageable, filter);
+        return  ResponseUtils.ok("Get Category paginate successfully", responses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<CategoryDetailResponse>> getDetail (@PathVariable("id") String id) {
+        CategoryDetailResponse categoryDetailResponse = this.categoryService.getCategoryById(id);
+        return ResponseUtils.ok("Get Category by " + id + " successfully", categoryDetailResponse);
+    }
+
+    @GetMapping("/{id}/tree")
+    public ResponseEntity<ApiResponse<CategoryTreeResponse>> getViewTree (@PathVariable("id") String id) {
+        CategoryTreeResponse categoryTreeResponse = this.categoryService.viewTreeCategory(id);
+        return  ResponseUtils.ok("Get Category view tree successfully", categoryTreeResponse);
+    }
+
+    @GetMapping("/trees")
+    public ResponseEntity<ApiResponse<List<CategoryTreeResponse>>> getTrees () {
+        List<CategoryTreeResponse> responses = this.categoryService.viewTreeCategories();
+        return ResponseUtils.ok("Get view trees categories successfully", responses);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<CategoryResponse>> updateCategory (
+            @PathVariable("id") String id,
+            @RequestBody @Valid CategoryUpdateRequest updateRequest) {
+        CategoryResponse response = this.categoryService.updateCategory(id, updateRequest);
+        return ResponseUtils.ok("Updated category successfully", response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> deleteCategory (@PathVariable("id") String id) {
+        this.categoryService.deleteCategory(id);
+        return ResponseUtils.ok("Deleted category successfully", null);
+    }
+}
