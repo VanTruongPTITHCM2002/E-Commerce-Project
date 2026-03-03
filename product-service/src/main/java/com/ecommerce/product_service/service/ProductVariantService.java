55d32f6a8726e000380442e2f51f3dd7005a67c3
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-03 11:45:18 +0700

    feat(product-service): add-function-product-variant-service

--- product-service/src/main/java/com/ecommerce/product_service/service/ProductVariantService.java
+++ product-service/src/main/java/com/ecommerce/product_service/service/ProductVariantService.java
@@ -1,4 +1,21 @@
 package com.ecommerce.product_service.service;
 
+import com.ecommerce.product_service.dto.request.ProductVariantRequest;
+import com.ecommerce.product_service.dto.request.ProductVariantUpdateRequest;
+import com.ecommerce.product_service.dto.response.PageResponse;
+import com.ecommerce.product_service.dto.response.ProductVariantResponse;
+import com.ecommerce.product_service.entity.ProductVariant;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
 public interface ProductVariantService {
+    ProductVariantResponse createProductVariant (ProductVariantRequest productVariantRequest);
+    PageResponse<ProductVariantResponse> getProductVariants (
+            Specification<ProductVariant> specification,
+            Pageable pageable,
+            String filter
+    );
+    ProductVariantResponse getProductVariantById (String productVariantId);
+    ProductVariantResponse updateProductVariant (String productVariantId, ProductVariantUpdateRequest updateRequest);
+    void deleteProductVariant (String productVariantId);
 }
