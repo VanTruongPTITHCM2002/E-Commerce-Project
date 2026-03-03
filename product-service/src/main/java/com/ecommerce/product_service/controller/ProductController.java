01a475e2abacda933310bec9e66dc2657fc94b9b
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-03 15:11:25 +0700

    feat(product-service): rename-url-mapping-in-product-controller

--- product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
@@ -1,6 +1,6 @@
 package com.ecommerce.product_service.controller;
 
-import com.ecommerce.product_service.common.MessageSuccess;
+import com.ecommerce.product_service.enums.MessageSuccess;
 import com.ecommerce.product_service.constants.RoleConstants;
 import com.ecommerce.product_service.dto.request.ProductRequest;
 import com.ecommerce.product_service.dto.response.PageResponse;
@@ -22,18 +22,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
-import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true,level = AccessLevel.PRIVATE)
 @Slf4j
@@ -41,8 +39,8 @@ public class ProductController {
 
     ProductService productService;
 
-    @GetMapping("/admin/products")
-    @PreAuthorize(RoleConstants.ROLE_ADMIN)
+    @GetMapping("/admin")
+//    @PreAuthorize(RoleConstants.ROLE_ADMIN)
     public ResponseEntity<ApiResponse<PageResponse<ProductAdminResponse>>>getProducts(
             @Filter Specification<Product> specification,
             @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
@@ -52,7 +50,7 @@ public class ProductController {
         return ResponseUtils.ok(MessageSuccess.PRODUCT_LIST_RETRIEVED.getMessage(),responses);
     }
 
-    @GetMapping("/products")
+    @GetMapping()
     public ResponseEntity<ApiResponse<Page<ProductResponse>>> getPublishProducts (
             @RequestParam(name = "size", defaultValue = "5") int size,
             @RequestParam(name = "page", defaultValue = "0") int page,
@@ -66,7 +64,7 @@ public class ProductController {
         return ResponseUtils.ok(MessageSuccess.PRODUCT_LIST_RETRIEVED.getMessage(),responses);
     }
 
-    @GetMapping("/products/paginate")
+    @GetMapping("/paginate")
     public ResponseEntity<ApiResponse<PageResponse<ProductAdminResponse>>> getProductsPaginate (
             @Filter Specification<Product> specification,
             @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
@@ -76,41 +74,41 @@ public class ProductController {
         return ResponseUtils.ok(MessageSuccess.PRODUCT_LIST_RETRIEVED.getMessage(), responses);
     }
 
-    @GetMapping("/admin/products/{productId}")
+    @GetMapping("/admin/{productId}")
     @PreAuthorize(RoleConstants.ROLE_ADMIN)
     public ResponseEntity<ApiResponse<ProductAdminResponse>> getProductById(@PathVariable("productId") String productId){
         ProductAdminResponse productResponse = this.productService.getProductById(productId);
         return ResponseUtils.ok(MessageSuccess.PRODUCT_RETRIEVED_SUCCESSFULLY.getMessage(), productResponse);
     }
 
-    @GetMapping("/products/{productId}")
+    @GetMapping("/{productId}")
     public ResponseEntity<ApiResponse<ProductResponse>> getProductDetail (@PathVariable String productId){
         ProductResponse response = this.productService.getProductDetail(productId);
         return ResponseUtils.ok(MessageSuccess.PRODUCT_RETRIEVED_SUCCESSFULLY.getMessage(), response);
     }
 
-    @PostMapping("/admin/products")
-    @PreAuthorize(RoleConstants.ROLE_ADMIN)
+    @PostMapping("/admin")
+//    @PreAuthorize(RoleConstants.ROLE_ADMIN)
     public ResponseEntity<ApiResponse<ProductAdminResponse>> insertProduct(@RequestBody @Valid ProductRequest productRequest){
         ProductAdminResponse response =  this.productService.insertProduct(productRequest);
         return ResponseUtils.create(MessageSuccess.PRODUCT_CREATED_SUCCESSFULLY.getMessage(), response);
     }
 
-    @PutMapping("/admin/products/{productId}")
+    @PutMapping("/admin/{productId}")
     @PreAuthorize(RoleConstants.ROLE_ADMIN)
     public  ResponseEntity<ApiResponse<ProductAdminResponse>> updateProduct (@PathVariable String productId, @RequestBody ProductRequest productRequest){
         ProductAdminResponse response = this.productService.updateProduct(productId,productRequest);
         return ResponseUtils.ok(MessageSuccess.PRODUCT_UPDATED_SUCCESSFULLY.getMessage(), response);
     }
 
-    @PutMapping("/products/{productId}")
+    @PutMapping("/{productId}")
     public ResponseEntity<ApiResponse<Boolean>> updateProductFromCart(@PathVariable String productId,
                                                                       @RequestParam int quantity, @RequestParam boolean isAdd){
         Boolean isSuccess = this.productService.updateProductFromCart(productId, quantity, isAdd);
         return ResponseUtils.ok(MessageSuccess.PRODUCT_UPDATED_SUCCESSFULLY.getMessage(), isSuccess);
     }
 
-    @DeleteMapping("/admin/products/{productId}")
+    @DeleteMapping("/admin/{productId}")
     @PreAuthorize(RoleConstants.ROLE_ADMIN)
     public  ResponseEntity<ApiResponse<Map<String,Boolean>>> deleteProduct (@PathVariable String productId){
         boolean isDeletedProduct = this.productService.deleteProduct(productId);
