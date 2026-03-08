abc2e595618952335e79dfe2c7c13eb9e721f9ad
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-08 15:03:08 +0700

    feat(product-service): add-field-in-role-constant

--- product-service/src/main/java/com/ecommerce/product_service/constants/RoleConstants.java
+++ product-service/src/main/java/com/ecommerce/product_service/constants/RoleConstants.java
@@ -2,4 +2,5 @@ package com.ecommerce.product_service.constants;
 
 public class RoleConstants {
     public static final String ROLE_ADMIN = "hasRole('ADMIN')";
+    public static final String ROLE_USER = "hasRole('USER')";
 }
