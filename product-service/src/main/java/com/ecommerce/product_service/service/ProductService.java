62acc07bb8d66b201d7b185277fadf0f9b2b0d51
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-08 16:05:02 +0700

    feat(product-service): add-create-update-delete-bulk-and-change-status-in-product-service

--- product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
+++ product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
@@ -2,26 +2,33 @@ package com.ecommerce.product_service.service;
 
 
 import com.ecommerce.product_service.dto.request.ProductRequest;
+import com.ecommerce.product_service.dto.request.ProductUpdateRequest;
+import com.ecommerce.product_service.dto.request.ProductUpdateStatusRequest;
 import com.ecommerce.product_service.dto.response.PageResponse;
 import com.ecommerce.product_service.dto.response.ProductAdminResponse;
 import com.ecommerce.product_service.dto.response.ProductResponse;
 import com.ecommerce.product_service.entity.Product;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 
 public interface ProductService {
     PageResponse<ProductAdminResponse> getAdminProducts(Specification<Product> specification, Pageable pageable, String filter);
-    PageResponse<ProductAdminResponse> getProducts (Specification<Product> specification, Pageable pageable, String filter);
-    Page<ProductResponse> getPublishProduct (Pageable pageable, BigInteger minPrice, BigInteger maxPrice, String keyword);
+    void createProductBulk(List<ProductRequest> productRequests);
+    void deleteProductBulk(List<String> productIds);
+    void updateProductBulk(Map<String, ProductUpdateRequest> productUpdateRequestMap);
+    PageResponse<ProductResponse> getProducts (Specification<Product> specification, Pageable pageable, String filter);
     ProductAdminResponse insertProduct(ProductRequest productDto);
     ProductAdminResponse getProductById(String productId);
     ProductResponse getProductDetail(String productId);
-    ProductAdminResponse updateProduct(String productId, ProductRequest productRequest);
+    ProductAdminResponse updateProduct(String productId, ProductUpdateRequest updateRequest);
     boolean updateProductFromCart(String productId, int quantity, boolean isAdd);
     boolean deleteProduct(String productId);
     Product checkExistsByIdAndReturn(String productId);
+    List<ProductResponse> getProductsByCategoryId (UUID categoryId);
+    void changeStatus(ProductUpdateStatusRequest productUpdateStatusRequest);
  }
