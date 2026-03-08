39997eb3a51fa325a1649d998ada7632fa253133
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-08 15:04:19 +0700

    feat(product-service): add-abstract-function-in-enum-entity-status

--- product-service/src/main/java/com/ecommerce/product_service/enums/EntityStatus.java
+++ product-service/src/main/java/com/ecommerce/product_service/enums/EntityStatus.java
@@ -1,8 +1,25 @@
 package com.ecommerce.product_service.enums;
 
 public enum EntityStatus {
-    ACTIVE,
-    INACTIVE,
-    OUT_OF_STOCK,
-    DRAFT
+    ACTIVE {
+        @Override
+        public boolean canTransitionTo(EntityStatus entityStatus) {
+            return entityStatus == INACTIVE || entityStatus == DELETED;
+        }
+    },
+    INACTIVE {
+        @Override
+        public boolean canTransitionTo(EntityStatus entityStatus) {
+            return entityStatus == ACTIVE || entityStatus == DELETED;
+        }
+    },
+//    OUT_OF_STOCK,
+//    DRAFT,
+    DELETED {
+    @Override
+    public boolean canTransitionTo(EntityStatus entityStatus) {
+        return entityStatus == INACTIVE;
+    }
+};
+    public abstract boolean canTransitionTo(EntityStatus entityStatus);
 }
