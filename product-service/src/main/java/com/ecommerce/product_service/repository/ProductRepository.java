e9eb0f146797d35a275feb2d436f9aa802240695
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-08 16:04:13 +0700

    feat(product-service): add-get-product-by-category-in-product-repo

--- product-service/src/main/java/com/ecommerce/product_service/repository/ProductRepository.java
+++ product-service/src/main/java/com/ecommerce/product_service/repository/ProductRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,4 +17,7 @@ import java.util.UUID;
 public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
     Optional<Product> findByName(String name);
     boolean existsByName(String name);
+
+    @Query("SELECT p FROM Product p WHERE p.category.id = :id")
+    List<Product> getProductsWithCategory (@Param("id") UUID categoryId);
 }
