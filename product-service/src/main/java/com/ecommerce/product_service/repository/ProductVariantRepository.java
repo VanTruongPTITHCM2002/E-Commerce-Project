4570d8e56ba908ba0a124045b0daee31856f5faa
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-08 15:59:39 +0700

    feat(product-service): add-find-by-product-in-product-variant-repo

--- product-service/src/main/java/com/ecommerce/product_service/repository/ProductVariantRepository.java
+++ product-service/src/main/java/com/ecommerce/product_service/repository/ProductVariantRepository.java
@@ -3,10 +3,14 @@ package com.ecommerce.product_service.repository;
 import com.ecommerce.product_service.entity.ProductVariant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductVariantRepository extends JpaRepository<ProductVariant, UUID>, JpaSpecificationExecutor<ProductVariant> {
     boolean existsBySku (String sku);
     boolean existsByVariantName (String variantName);
+    @Query("SELECT pv from ProductVariant pv where pv.product.id = :productId")
+    List<ProductVariant> findByProduct (UUID productId);
 }
