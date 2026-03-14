e04a88aeda98d979179f9dd2102bd2a616f949e8
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-14 18:35:13 +0700

    feat(product-service): add-bean-mapping-to-update-in-brand-mapper

--- product-service/src/main/java/com/ecommerce/product_service/mapper/BrandMapper.java
+++ product-service/src/main/java/com/ecommerce/product_service/mapper/BrandMapper.java
@@ -4,9 +4,7 @@ import com.ecommerce.product_service.dto.request.BrandRequest;
 import com.ecommerce.product_service.dto.request.BrandUpdateRequest;
 import com.ecommerce.product_service.dto.response.BrandResponse;
 import com.ecommerce.product_service.entity.Brand;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface BrandMapper {
@@ -14,7 +12,9 @@ public interface BrandMapper {
     Brand toEntity (BrandRequest brandRequest);
 
     @Mapping(source = "code", target = "code")
+    @Mapping(source = "entityStatus", target = "status")
     BrandResponse toResponse (Brand brand);
 
-    Brand toUpdate (@MappingTarget Brand brand, BrandUpdateRequest brandUpdateRequest);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void toUpdate (@MappingTarget Brand brand, BrandUpdateRequest brandUpdateRequest);
 }
