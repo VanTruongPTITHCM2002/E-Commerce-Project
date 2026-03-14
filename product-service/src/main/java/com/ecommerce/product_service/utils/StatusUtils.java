1d4dec2f3ca120940dded2995509fb44b32ae1fe
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-03-14 18:29:45 +0700

    feat(product-service): add-implement-logic-transition-status-in-status-utils

--- product-service/src/main/java/com/ecommerce/product_service/utils/StatusUtils.java
+++ product-service/src/main/java/com/ecommerce/product_service/utils/StatusUtils.java
@@ -1,4 +1,15 @@
 package com.ecommerce.product_service.utils;
 
+import com.ecommerce.product_service.enums.EntityStatus;
+
 public class StatusUtils {
+    public static boolean transitionStatus(String fromStatus, String toStatus) {
+         EntityStatus oldStatus = getEntityStatus(fromStatus);
+         EntityStatus newStatus = getEntityStatus(toStatus);
+         return oldStatus.canTransitionTo(newStatus);
+    }
+
+    public static EntityStatus getEntityStatus(String status) {
+        return EntityStatus.valueOf(status);
+    }
 }
